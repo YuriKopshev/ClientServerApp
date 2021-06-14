444f01981973c6278708c174f7e99e1939ac3df5
“Yuri <yura.kopshev@gmail.com>
2021-06-14 14:53:10 +0400

    Changed Client.java -"localhost" to "netology.homework" and Server.java name port to PORT

--- src/main/java/ru/netology/Client.java
+++ src/main/java/ru/netology/Client.java
@@ -8,21 +8,21 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket clientSocket = new Socket("127.0.0.1", Server.port);
+        try (Socket clientSocket = new Socket("netology.homework", Server.PORT);
              InputStream from = clientSocket.getInputStream();
              OutputStream to = clientSocket.getOutputStream();
              BufferedReader reader = new BufferedReader(new InputStreamReader(from));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(to))) {
-           while (true) {
-               String question;
-               question = reader.readLine();
-               System.out.println("server talk: " + question + "\n");
+            while (true) {
+                String question;
+                question = reader.readLine();
+                System.out.println("server talk: " + question + "\n");
 
-               writer.write("Yuri" + "\n");
-               writer.flush();
-               writer.write("no" + "\n");
-               writer.flush();
-           }
+                writer.write("Yuri" + "\n");
+                writer.flush();
+                writer.write("no" + "\n");
+                writer.flush();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -5,38 +5,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    final static int port = 8181;
-
+    final static int PORT = 8181;
 
 
     public static void main(String[] args) {
 
-        try (ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(PORT);
              Socket clientSocket = serverSocket.accept();
              InputStream from = clientSocket.getInputStream();
              OutputStream to = clientSocket.getOutputStream();
              BufferedReader reader = new BufferedReader(new InputStreamReader(from));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(to))) {
-          while (true) {
-              writer.write("Write your name..." + "\n");
-              writer.flush();
-              String name = reader.readLine();
-              writer.write("  Are you child? (yes/no)" + "\n");
-              writer.flush();
+            while (true) {
+                writer.write("Write your name..." + "\n");
+                writer.flush();
+                String name = reader.readLine();
+                writer.write("  Are you child? (yes/no)" + "\n");
+                writer.flush();
+                String answer = reader.readLine();
 
-
-              String answer = reader.readLine();
-
-              if (answer.equals("yes")) {
-                  writer.write("Welcome to the kids area," + name + " Let's play!" + "\n" );
-              } else {
-                  writer.write("Welcome to the adult zone, " + name + " Have a good rest, or a good working day!" + "\n");
-                  break;
-              }
-              writer.flush();
-
-
-          }
+                if (answer.equals("yes")) {
+                    writer.write("Welcome to the kids area," + name + " Let's play!" + "\n");
+                } else {
+                    writer.write("Welcome to the adult zone, " + name + " Have a good rest, or a good working day!" + "\n");
+                    break;
+                }
+                writer.flush();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
